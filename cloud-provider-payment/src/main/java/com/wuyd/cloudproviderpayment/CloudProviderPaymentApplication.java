d64dcf3dfbea014916e614bc45082d1bdb4ae77d
wuyd <wuydit@163.com>
2020-09-09 18:26:12 +0800

    first commit

---
+++ cloud-provider-payment/src/main/java/com/wuyd/cloudproviderpayment/CloudProviderPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.wuyd.cloudproviderpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProviderPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPaymentApplication.class, args);
+    }
+
+}
