ae2b00b9c442239e095c9be83af2622439b15ced
wuyd <wuydit@163.com>
2020-09-17 16:54:23 +0800

    添加和删除接口

--- cloud-provider-payment9001/src/main/java/com/wuyd/cloudproviderpayment/controller/PaymentController.java
+++ cloud-provider-payment9001/src/main/java/com/wuyd/cloudproviderpayment/controller/PaymentController.java
@@ -1,10 +1,15 @@
 package com.wuyd.cloudproviderpayment.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.wuyd.cloudproviderpayment.dto.CommonResult;
+import com.wuyd.cloudproviderpayment.entity.PaymentEntity;
+import com.wuyd.cloudproviderpayment.service.PaymentService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -14,9 +19,30 @@ import org.springframework.web.bind.annotation.RestController;
  * @author wuYd
  * @since 2020-09-17
  */
+@Slf4j
+@AllArgsConstructor
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/")
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("payment")
+    public ResponseEntity<CommonResult<Boolean>> create(@RequestBody PaymentEntity paymentEntity){
+        if (paymentService.save(paymentEntity)) {
+            return ResponseEntity.ok(new CommonResult<>(0, "success",Boolean.TRUE));
+        }
+        return ResponseEntity.ok(new CommonResult<>(1, "No",Boolean.FALSE));
+    }
+
+    @GetMapping("payment/{id}")
+    public ResponseEntity<CommonResult<PaymentEntity>> create(@PathVariable Long id){
+        PaymentEntity paymentEntity = paymentService.getById(id);
+        if (paymentEntity != null) {
+            return ResponseEntity.ok(new CommonResult<>(0, "success",paymentEntity));
+        }
+        return ResponseEntity.ok(new CommonResult<>(1, "No"));
+    }
 
 }
 
