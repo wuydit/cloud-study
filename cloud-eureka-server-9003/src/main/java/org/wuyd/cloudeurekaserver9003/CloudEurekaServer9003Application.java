e61892e128ab3fafaee1d6dc8eae0932c2eb49f9
wuyd <wuydit@163.com>
2020-09-21 23:36:12 +0800

    eureka

---
+++ cloud-eureka-server-9003/src/main/java/org/wuyd/cloudeurekaserver9003/CloudEurekaServer9003Application.java
@@ -0,0 +1,13 @@
+package org.wuyd.cloudeurekaserver9003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaServer9003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaServer9003Application.class, args);
+    }
+
+}
---
+++ cloud-eureka-server-9003/src/test/java/org/wuyd/cloudeurekaserver9003/CloudEurekaServer9003ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wuyd.cloudeurekaserver9003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaServer9003ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
