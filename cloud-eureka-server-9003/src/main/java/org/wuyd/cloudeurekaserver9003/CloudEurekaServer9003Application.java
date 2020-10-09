6e92ed10a93cfda81adc830fcbcd2b3a3dc33e41
wuydit <wuydit@163.com>
2020-10-09 18:14:37 +0800

    eureka server

--- cloud-eureka-server-9003/src/main/java/org/wuyd/cloudeurekaserver9003/CloudEurekaServer9003Application.java
+++ cloud-eureka-server-9003/src/main/java/org/wuyd/cloudeurekaserver9003/CloudEurekaServer9003Application.java
@@ -2,7 +2,9 @@ package org.wuyd.cloudeurekaserver9003;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class CloudEurekaServer9003Application {
 
